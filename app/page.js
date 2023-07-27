670deb3b2512e9340de9dffe740b44afff0b57bf
Daniel <danielahumada071@gmail.com>
2023-07-27 10:50:49 -0500

    add a start button

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <div className="flex flex-col items-center">
+        <div class="title">
+          <h1 class="font-sans text-7xl">welcome to my app</h1>
+          <p className="italic">the app to do whatever i want</p>
+        </div>
+          
+        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Start
+        </button>
+
+      </div>
     </main>
   )
 }
