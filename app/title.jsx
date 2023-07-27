3d106364be5ea6c0f591b7b9228a2d0be15870c3
Daniel <danielahumada071@gmail.com>
2023-07-27 11:33:34 -0500

    add a img

---
+++ app/title.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ImageTitle() {
+  return (
+    <div className="landing-page">
+      <img src="/stock.webp" alt="Imagen de fondo" className="img-bg" />
+
+      <div className="absolute inset-0 flex items-center justify-center flex-col text-white text-center">
+        <h1 className="font-sans text-7xl">welcome to my app</h1>
+        <p className="italic">the app to do whatever i want</p>
+
+        <button className="mt-8 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Start
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageTitle;
