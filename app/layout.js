a5566688163cfb2eb89e9fa3c43c8ea11d267f38
Daniel <danielahumada071@gmail.com>
2023-08-08 19:17:17 -0500

    update dependencies

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigator/>
+        <Navigator />
         {children}
       </body>
     </html>
-  )
+  );
 }
