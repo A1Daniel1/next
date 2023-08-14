7a0b510b22ee6df96e035100b99ba2ecbb187085
Daniel <danielahumada071@gmail.com>
2023-08-14 12:22:26 -0500

    refactoring the main page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,23 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
       <div className="flex flex-col items-center">
-        <div class="title">
-          <h1 class="font-sans text-7xl">Climate app</h1>
-          <p className="italic">Just select the city, and see the Climate</p>
-        </div>
-          
-        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-          Start
-        </button>
+        <div className="title items-center">
+          <h1 className="capitalize font-sans text-9xl font-semibold">Climate app</h1>
 
+          <p className="italic text-center text-2xl pt-6">Just select the city, and see the Climate</p>
+
+          <div className="pt-4 flex justify-center items-center">
+            <input
+              type="text"
+              placeholder="city"
+              className="py-2 rounded-md border border-gray-300 bg-black"
+            />
+          </div>
+  
+        </div>
       </div>
     </main>
-  )
+  );
 }
