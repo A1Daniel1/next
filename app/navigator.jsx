535803f43429b3c3528dc7f48b7c0e714980010c
Daniel <danielahumada071@gmail.com>
2023-08-14 10:52:19 -0500

    fix navigator class

--- app/navigator.jsx
+++ app/navigator.jsx
@@ -3,24 +3,21 @@ import Link from "next/link";
 
 function Navigator() {
     return (
-        <>
-            <nav class="bg-gray-800 py-4">
-                <div class="container mx-auto px-4">
-                    <div class="flex justify-between items-center">
-                        <div class="text-white font-bold text-xl">
-                            Climate App
-                        </div>
-                        <div class="hidden md:flex space-x-4">
-                            <Link href="/" class="text-white hover:text-gray-400">Home</Link>
-                            <Link href="/about" class="text-white hover:text-gray-400">about</Link>
-                            <Link href="/service" class="text-white hover:text-gray-400">service</Link>
-                        </div>
+        <nav className="bg-gray-800 py-4">
+            <div className="container mx-auto px-4">
+                <div className="flex justify-between items-center">
+                    <div className="text-white font-bold text-xl">
+                        Climate App
+                    </div>
+                    <div className="hidden md:flex space-x-4">
+                        <Link href="/" className="text-white hover:text-gray-400">Home</Link>
+                        <Link href="/about" className="text-white hover:text-gray-400">about</Link>
+                        <Link href="/service" className="text-white hover:text-gray-400">service</Link>
                     </div>
                 </div>
-            </nav>
-
-        </>
-    )
+            </div>
+        </nav>
+    );
 }
 
 
