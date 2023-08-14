3df793b6ad080ace83eb018addd348ce1d1d21cb
Daniel <danielahumada071@gmail.com>
2023-08-14 10:58:10 -0500

    fix the main page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center">
         <div class="title">
-          <h1 class="font-sans text-7xl">welcome to my app</h1>
-          <p className="italic">the app to do whatever i want</p>
+          <h1 class="font-sans text-7xl">Climate app</h1>
+          <p className="italic">Just select the city, and see the Climate</p>
         </div>
           
         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
